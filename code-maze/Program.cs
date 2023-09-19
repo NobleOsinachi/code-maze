b5caf5781e29dfae1ca54dfeb7808c6f9d81c68d
Noble Osinachi <nobleosinachi98@gmail.com>
2023-09-19 11:27:35 +0100

    Add Student Class

--- code-maze/Program.cs
+++ code-maze/Program.cs
@@ -12,6 +12,26 @@ namespace code_maze
         {
             Console.WriteLine("Hello World");
             Console.ReadKey();
+
+
+            List<Student> firstYearStudents = new List<Student>
+            {
+                new Student {Id=1, Name="Mary", Course="Physics", Grade="A" },
+                new Student {Id=2, Name="John", Course="Geography", Grade="B" },
+                new Student {Id=3, Name="Peter", Course="Biology", Grade="C" },
+                new Student {Id=4, Name="David", Course="Mathematics", Grade="D" },
+            };
+
+            Func<Student, string> flesh = (student) => student.Name+" scored "+student.Grade + " in "+student.Course;
+
         }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Grade { get; set; }
     }
 }
